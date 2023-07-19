659862014fe369c93059373196f813fcd1eaddda
Bodovuse <tamzin.davies83@gmail.com>
2023-07-19 23:27:15 +0100

    A

--- code/level.py
+++ code/level.py
@@ -4,12 +4,13 @@ from tile import Tile
 from player import Player
 from debug import debug
 
+
 class Level:
     def __init__(self):
         # get the display surface (self.screen)
         self.display_surface = pygame.display.get_surface()
 
-        # sprite group et up
+        # sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
@@ -25,35 +26,38 @@ class Level:
                 # putting rock sprite on map
                 if col == "x":
                     Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                #place player on map
+                # place player on map
                 if col == 'p':
-                    self.player = Player((x,y), [self.visible_sprites],self.obstacle_sprites)
+                    self.player = Player(
+                        (x, y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
 
-#camera
-#sorting sprites by their y coord (this decides sprites being infront/behind)
-#offset the world to wherever the player is
+# camera
+# sorting sprites by their y coord (this decides sprites being infront/behind)
+# offset the world to wherever the player is
+
+
 class YSortCameraGroup(pygame.sprite.Group):
     def __init__(self):
 
-        #general set up
+        # general set up
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.half_width = self.display_surface.get_size()[0] // 2
         self.half_height = self.display_surface.get_size()[1] // 2
-        self.offset = pygame.math.Vector2(100,200)
+        self.offset = pygame.math.Vector2(100, 200)
 
     def custom_draw(self, player):
 
-        #getting the offset
+        # getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        #for sprite in self.sprites():
-        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
